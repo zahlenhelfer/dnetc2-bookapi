a3046f732c4eae67006dfca34c87358cd5a1a611
Marcus Ross <marcus.ross@zahlenhelfer.de>
2017-12-06 12:11:48 +0100

    added Migration-Support

---
+++ Migrations/20171205170514_initalMigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace myBookAPI.Migrations
+{
+    public partial class initalMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Author = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    CoverUrl = table.Column<string>(type: "TEXT", nullable: true),
+                    Isbn = table.Column<string>(type: "TEXT", maxLength: 13, nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false),
+                    Rating = table.Column<double>(type: "REAL", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
